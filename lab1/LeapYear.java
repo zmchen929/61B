4492bcb723042f6970bac8b1e32187032903f56a
HelenChen <zmchen929@gmail.com>
2018-10-29 22:05:08 -0700

    Leapyear done

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,17 @@
 /** Class that determines whether or not a year is a leap year.
  *  @author YOUR NAME HERE
  */
+
+/**judge is year is a leap Year
+return true or false
+*/
 public class LeapYear {
+   public static boolean isLeapYear(int year){
+     if(year%400==0 || (year%4==0 && year%100!=0)){
+       return true;
+     }
+     return false;
+   }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
@@ -30,4 +40,3 @@ public class LeapYear {
         }
     }
 }
-
